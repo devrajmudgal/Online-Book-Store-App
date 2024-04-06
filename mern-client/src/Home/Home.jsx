2644ddcbaafcfa7561bb9dab6aabfc7934e4b52c
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2024-04-07 00:23:11 +0530

    Creating Banner and it contains several book Cards

--- mern-client/src/Home/Home.jsx
+++ mern-client/src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../components/Banner";
+import FavouriteBooks from "./FavouriteBooks";
 
 const Home = () => {
   return (
-    <div className="">
-      <div className="h-screen">Home</div>
-      <div className="h-screen bg-red-600"></div>
+    <div>
+      <Banner />
+      <FavouriteBooks />
     </div>
   );
 };
