8e5a758415de110a43b0fe1cab2d12748172b483
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2024-04-24 21:25:16 +0530

    made some change

--- mern-client/src/components/About.jsx
+++ mern-client/src/components/About.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 const About = () => {
   return (
     <div className="mt-28 px-4 lg:px-24">
-      <h1 className='text-5xl mb-20 font-bold text-center'>Our Mission</h1>
-        <p className='font-normal text-gray-700 dark:text-gray-400'>At EngiReads, our mission is to empower engineers, students, and enthusiasts with the knowledge and resources they need to thrive in the ever-evolving world of engineering. We believe that access to quality educational materials is fundamental to the advancement of engineering innovation and excellence.</p>
-        
-        <h3>Commitment to Quality Education</h3>
-        <p>We are committed to curating a diverse collection of engineering books that encompasses a wide range of disciplines, from mechanical and electrical engineering to civil and environmental engineering, and beyond. Our carefully selected titles are chosen for their relevance, accuracy, and educational value, ensuring that our customers have access to the latest advancements and foundational principles in their field of study.</p>
+      <h1 className='text-5xl mb-20 font-bold text-center'>Our <span className='text-blue-700'>Mission</span> </h1>
+      <p className='font-normal text-gray-700 dark:text-gray-400'>At EngiReads, our mission is to empower engineers, students, and enthusiasts with the knowledge and resources they need to thrive in the ever-evolving world of engineering. We believe that access to quality educational materials is fundamental to the advancement of engineering innovation and excellence.</p>
 
-        <h3>Accessibility and Affordability</h3>
-        <p>We understand the financial constraints often faced by students and professionals alike. That's why we strive to make our books accessible and affordable to all. Through competitive pricing, special discounts, and occasional promotions, we aim to break down barriers to education and make learning resources more attainable for everyone.</p>
-        
-        <h3>Empowering the Engineering Community</h3>
-        <p>Beyond simply selling books, we are passionate about fostering a vibrant and supportive engineering community. Through partnerships with educational institutions, participation in industry events, and sponsorship of engineering initiatives, we seek to contribute to the growth and development of future generations of engineers. We believe that by empowering individuals with knowledge and opportunities, we can collectively drive positive change and innovation in the world.</p>
+      <h3>Commitment to Quality Education</h3>
+      <p>We are committed to curating a diverse collection of engineering books that encompasses a wide range of disciplines, from mechanical and electrical engineering to civil and environmental engineering, and beyond. Our carefully selected titles are chosen for their relevance, accuracy, and educational value, ensuring that our customers have access to the latest advancements and foundational principles in their field of study.</p>
 
-        <h3>Continuous Improvement</h3>
-        <p>We are committed to continuous improvement and innovation in everything we do. Whether it's enhancing our website's user experience, expanding our book collection to cover emerging technologies, or refining our customer service processes, we are always striving to raise the bar and exceed our customers' expectations.</p>
+      <h3>Accessibility and Affordability</h3>
+      <p>We understand the financial constraints often faced by students and professionals alike. That's why we strive to make our books accessible and affordable to all. Through competitive pricing, special discounts, and occasional promotions, we aim to break down barriers to education and make learning resources more attainable for everyone.</p>
 
-        <h3>Your Partner in Engineering Excellence</h3>
-        <p>At EngiReads, we are more than just a retailer – we are your partner in engineering excellence. We are dedicated to supporting your educational and professional journey every step of the way, providing you with the tools and resources you need to succeed in your endeavors.
+      <h3>Empowering the Engineering Community</h3>
+      <p>Beyond simply selling books, we are passionate about fostering a vibrant and supportive engineering community. Through partnerships with educational institutions, participation in industry events, and sponsorship of engineering initiatives, we seek to contribute to the growth and development of future generations of engineers. We believe that by empowering individuals with knowledge and opportunities, we can collectively drive positive change and innovation in the world.</p>
+
+      <h3>Continuous Improvement</h3>
+      <p>We are committed to continuous improvement and innovation in everything we do. Whether it's enhancing our website's user experience, expanding our book collection to cover emerging technologies, or refining our customer service processes, we are always striving to raise the bar and exceed our customers' expectations.</p>
+
+      <h3>Your Partner in Engineering Excellence</h3>
+      <p>At EngiReads, we are more than just a retailer – we are your partner in engineering excellence. We are dedicated to supporting your educational and professional journey every step of the way, providing you with the tools and resources you need to succeed in your endeavors.
 
         Join us in our mission to inspire, educate, and empower the engineers of tomorrow. Together, let's build a brighter future through the power of knowledge and innovation.
 
-        </p>
-      </div>
+      </p>
+    </div>
   )
 }
 
