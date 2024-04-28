4d107df7debc26adfd4b3d3af0a864ebe0275bc5
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2024-04-28 12:20:17 +0530

    updated nav

--- mern-client/src/components/Navbar.jsx
+++ mern-client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
 
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -35,15 +35,14 @@ const Navbar = () => {
     { link: "Home", path: "/" },
     { link: "About", path: "/about" },
     { link: "Shop", path: "/shop" },
-    // { link: "Sell Your Book", path: "/admin/dashboard" },
-    { link: "Cart", path: "/cart" },
+    { link: "Sell Your Book", path: "/admin/dashboard" },
+    // { link: "Cart", path: "/cart" },
   ];
   return (
     <header className="w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
       <nav
-        className={`py-4 lg:px-24 px-4 ${
-          isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""
-        }`}
+        className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""
+          }`}
       >
         <div className="flex justify-between items-center text-base gap-8">
           {/* logo */}
@@ -74,7 +73,7 @@ const Navbar = () => {
             <button className="hidden">
               <FaBarsStaggered className="w-5 hover:text-blue-700" />
             </button>
-            
+
           </div>
 
           {/* menu btn for mobile devices */}
@@ -94,9 +93,8 @@ const Navbar = () => {
 
         {/* navigation for sm devices */}
         <div
-          className={`space-y-4 px-4 mt-16 py-7 bg-blue-700 ${
-            isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
-          }`}
+          className={`space-y-4 px-4 mt-16 py-7 bg-blue-700 ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
+            }`}
         >
           {navItems.map(({ link, path }) => (
             <Link
@@ -106,9 +104,9 @@ const Navbar = () => {
             >
               {link}
             </Link>
-           
+
           ))}
-        </div> 
+        </div>
       </nav>
     </header>
   );
